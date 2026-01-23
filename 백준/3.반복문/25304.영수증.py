17a913e8ca3617db87fd995c43db1a4067f4c814
dudwltjs <dudwltjs@gmail.com>
2026-01-23 21:25:01 +0900

    1

---
+++ 백준/3.반복문/25304.영수증.py
@@ -0,0 +1,12 @@
+X = int(input())
+n = int(input())
+s = 0
+
+for i in range(n) : 
+    a, b = map(int, input().split())
+    s += a * b
+
+if s == X : 
+    print('Yes')
+else :
+    print('No')
---
+++ 백준/3.반복문/8393.합.py
@@ -0,0 +1,8 @@
+a = int(input())
+
+sum_a = 0
+
+for i in range(1, a+1) :
+     sum_a += i
+
+print(sum_a)
