9c7b2eb0f3b994aacbce6c823cd4a3706e7fe137
dudwltjs <dudwltjs@gmail.com>
2026-01-26 00:16:20 +0900

    1

---
+++ 백준/11.시간 복잡도/24262.수행시간1.py
@@ -0,0 +1,7 @@
+# MenOfPassion(A[], n) {
+#     i = ⌊n / 2⌋;
+#     return A[i]; # 코드1
+# }
+
+print(1) #수행횟수
+print(0) #수행횟수 상수
---
+++ 백준/11.시간 복잡도/24264.수행시간3.py
@@ -0,0 +1,10 @@
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         for j <- 1 to n
+#             sum <- sum + A[i] × A[j]; # 코드1
+#     return sum;
+# }
+
+print(int(input()) ** 2) #입력값 제곱만큼
+print(2) #n^2
