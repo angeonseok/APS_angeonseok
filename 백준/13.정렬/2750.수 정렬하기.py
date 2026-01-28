680de4664219984547e38a6d4613b7933f591414
dudwltjs <dudwltjs@gmail.com>
2026-01-28 16:42:46 +0900

    1

--- 백준/13.정렬/2750.수 정렬하기.py
+++ 백준/13.정렬/2750.수 정렬하기.py
@@ -10,7 +10,7 @@
 a = int(input())        #입력 받고
 c = []
 
-for i in range(a):      #입력만큼 다시 입력하고 그거 리스트에 박고
+for i in range(a):      #입력만큼 다시 순차적으로 입력하고 그거 리스트에 박고
     b = int(input())     
     c.append(b)         #정렬하고
 
