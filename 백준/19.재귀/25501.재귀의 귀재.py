d49a94fbb68c9ebfa0acf02d683a5a0ba9a51573
dudwltjs <dudwltjs@gmail.com>
2026-01-29 22:19:30 +0900

    1

--- 백준/19.재귀/25501.재귀의 귀재.py
+++ 백준/19.재귀/25501.재귀의 귀재.py
@@ -19,17 +19,19 @@
 
 
 def recursion(s, l, r):
-    cnt = 0
+    global cnt                  #글로벌로 카운트값 유지
+    cnt +=1
     if l >= r: 
-        return cnt
+        return 1                #지가 바꿔놓고 까먹는 사람이 있다?
     elif s[l] != s[r]: 
         return 0
     else: 
-        cnt += 1
         return recursion(s, l+1, r-1)
 
 def isPalindrome(s):
     return recursion(s, 0, len(s)-1)
 
-print('ABBA:', isPalindrome('ABBAABBA'))
-print('ABC:', isPalindrome('ABC'))
+for _ in range(int(input())):
+    cnt = 0
+    s = input().rstrip()
+    print(isPalindrome(s), cnt)
