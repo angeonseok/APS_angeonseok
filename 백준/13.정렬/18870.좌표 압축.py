5a1ace8935310001dadef4d501372b31833cd68d
dudwltjs <dudwltjs@gmail.com>
2026-02-01 23:56:39 +0900

    1

--- 백준/13.정렬/18870.좌표 압축.py
+++ 백준/13.정렬/18870.좌표 압축.py
@@ -15,4 +15,11 @@ input = sys.stdin.readline
 n = int(input().rstrip())
 num = list(map(int, input().split()))
 
+#중복 지우고 정렬을 하자
 set_num = sorted(set(num))
+
+#젤 작은 놈부터 0 가진 딕셔너리
+dict_num = dict(zip(set_num, range(len(set_num))))  
+
+for i in range(len(num)):
+    print(dict_num[num[i]], end=" ")            #ㅇㅇ
