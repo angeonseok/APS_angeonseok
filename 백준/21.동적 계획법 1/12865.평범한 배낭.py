0da0ecacc84447dceb37dcfc3bc2293db9bc0b44
dudwltjs <dudwltjs@gmail.com>
2026-02-05 13:37:53 +0900

    1

--- 백준/21.동적 계획법 1/12865.평범한 배낭.py
+++ 백준/21.동적 계획법 1/12865.평범한 배낭.py
@@ -9,12 +9,21 @@
 #출력
 # 한 줄에 배낭에 넣을 수 있는 물건들의 가치합의 최댓값을 출력한다.
 
-# swea 4869
 import sys
 input = sys.stdin.readline
 
 n, k = map(int,input().split())
 
-     
+#이차원 배열로 자료 받기
+item_list = [map(int, input().split()) for _ in range(n)]
 
+#우리는 k값에 따라 결과가 달라진다
+dp = [0] * (k + 1)
 
+#딱 맞게 넣거나, 이전 결과 + 무게 한도 안걸리는 놈 추가
+for w, v in item_list:
+    for i in range(k, w -1, -1):
+        dp[i] = max(dp[i], dp[i-w] + v)
+
+#답은 k가 결정한다
+print(dp[k])
