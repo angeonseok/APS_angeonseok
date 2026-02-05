e62a392ccdbbd845a46d85fff20922f7992d0fdd
dudwltjs <dudwltjs@gmail.com>
2026-02-05 17:17:09 +0900

    1

--- 백준/18.심화 2(완)/20920.영단어 암기는 괴로워.py
+++ 백준/18.심화 2(완)/20920.영단어 암기는 괴로워.py
@@ -15,17 +15,25 @@
 
 import sys
 input = sys.stdin.readline
+from collections import Counter
 
 n, m = map(int, input().split())
 
-#일단 이 풀이 아님
+#입력 받을 때 걸러
 word = []
 for i in range(n):
     w = input().rstrip()
     if len(w) >= m:
         word.append(w)
 
-word.sort(key = lambda x : (word.count(x),len(x), x))
+#카운터쓰면 카운팅 + 중복제거
+word_c = Counter(word)
+
+#딕셔너리 형태라 key 뽑아오고
+word = list(word_c.keys())
+
+#1. 빈도수 높은 순 2. 길이가 긴 거 3. 사전 순
+word.sort(key = lambda x : (-word_c[x], -len(x), x))
 
 for i in word:
     print(i)
