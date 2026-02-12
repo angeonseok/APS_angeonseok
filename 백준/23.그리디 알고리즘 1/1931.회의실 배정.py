a70e949f484ab682a49f1b4bbc3abe186d068ddf
ags <95411351+angeonseok@users.noreply.github.com>
2026-02-12 23:55:52 +0900

    1

--- 백준/23.그리디 알고리즘 1/1931.회의실 배정.py
+++ 백준/23.그리디 알고리즘 1/1931.회의실 배정.py
@@ -9,13 +9,23 @@
 """
 
 import sys
-# input = sys.stdin.readline
-sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
 
 n = int(input())
-tt = []
-for _  in range(n):
-    tt.append(list(map(int, input().split())))
+tt = [list(map(int, input().split())) for _ in range(n)]
 
-tt.sort()
-print(tt)
+#종료시간 기준 오름차순. 같다면 시작시간 기준 오름차순
+tt.sort(key=lambda x : (x[1], x[0]))
+
+cnt = 0
+
+#종료시간 저장용
+end = 0
+
+for s, e in tt:
+    #종료시간 다음 회의 바로 잡기
+    if s >= end:
+        cnt += 1
+        end = e #잡으면 그에 맞게 종료시간 변동
+
+print(cnt)
