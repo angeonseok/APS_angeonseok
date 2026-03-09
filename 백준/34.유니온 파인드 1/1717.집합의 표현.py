01ec3652c628a98e6c9af5ddd21671f4279a99a4
angeonseok <dudwltjs@gmail.com>
2026-03-09 16:50:59 +0900

    1

--- 백준/34.유니온 파인드 1/1717.집합의 표현.py
+++ 백준/34.유니온 파인드 1/1717.집합의 표현.py
@@ -14,13 +14,16 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
+#자기 자신을 부모로 시작해보자
 parent = [i for i in range(n+1)]
 
+#x의 루트를 경로를 압축하면서 찾자
 def find(x):
     if parent[x] != x:
         parent[x] = find(parent[x])
     return parent[x]
 
+#서로의 루트가 다를 경우 통합하자(b루트를 a루트 밑으로)
 def union(a, b):
     rootA = find(a)
     rootB = find(b)
--- 백준/34.유니온 파인드 1/1976.여행 가자.py
+++ 백준/34.유니온 파인드 1/1976.여행 가자.py
@@ -10,13 +10,7 @@
 """
 
 import sys
-# input = sys.stdin.readline
-sys.stdin = open("input.txt")
-
-n = int(input())
-m = int(input())
-
-parent = [i for i in range()]
+input = sys.stdin.readline
 
 def find(x):
     if parent[x] != x:
@@ -30,5 +24,31 @@ def union(a, b):
     if rootA != rootB:
         parent[rootB] = rootA
     
-for _ in range(m):
-    
+n = int(input())
+m = int(input())
+
+parent = [i for i in range(n+1)]
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+#연결 됨(1) 상태면 인덱스 확인해서 같은 집합으로 묶기
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1:
+            union(i+1, j+1)
+
+#루트노드간 비교를 통해 주어진 도시간 연결 확인
+t_list = list(map(int,input().split()))
+for i in range(len(t_list)):
+    t_list[i] = find(t_list[i])
+
+#루트노드가 전부 같으면 이동가능
+flag = True
+for j in range(m-1):
+    if t_list[j] != t_list[j+1]:
+        flag = False
+        break
+
+if flag:
+    print('YES')
+else:
+    print('NO')
