edb2d29310c268768eefe376c101f0ad1a64432b
angeonseok <dudwltjs@gmail.com>
2026-03-11 17:58:57 +0900

    1

--- 백준/22.누적 합(완)/16139.인간-컴퓨터 상호작용.py
+++ 백준/22.누적 합(완)/16139.인간-컴퓨터 상호작용.py
@@ -25,10 +25,10 @@ for order, ch in enumerate(text):
     text_change[ord(ch)-97][order] += 1
 
 #누적합으로 각 문자의 등장 횟수 저장
-perfix = [[0] * (len(text) + 1) for _ in range(26)]
+prefix = [[0] * (len(text) + 1) for _ in range(26)]
 for x in range(26):
     for y in range(1, len(text) + 1):
-        perfix[x][y] = text_change[x][y-1] + perfix[x][y-1]
+        prefix[x][y] = text_change[x][y-1] + prefix[x][y-1]
 
 out = []
 for _ in range(q):
@@ -36,7 +36,7 @@ for _ in range(q):
     a = ord(a) - 97 
     b = int(b)
     c = int(c) + 1
-    out.append(str(perfix[a][c] - perfix[a][b]))
+    out.append(str(prefix[a][c] - prefix[a][b]))
 
 #join으로 한번에 출력
 print("\n".join(out))
--- 백준/22.누적 합(완)/2559.수열.py
+++ 백준/22.누적 합(완)/2559.수열.py
@@ -24,13 +24,13 @@ n, k = map(int, input().split())
 temp = list(map(int, input().split()))
 
 #일단 누적합 생성
-perfix = [0] * (n + 1)
+prefix = [0] * (n + 1)
 for i in range(1, n + 1):
-    perfix[i] = perfix[i-1] + temp[i-1]  
+    prefix[i] = prefix[i-1] + temp[i-1]  
 
 #누적합 간의 차를 이용. 걍 이중 for 뇌빼고 돌렸다 터짐.
 sub = [0] * (n - k + 1)
 for i in range(n-k+1):
-    sub[i] = perfix[i+k] - perfix[i]
+    sub[i] = prefix[i+k] - prefix[i]
 
 print(max(sub))
