d2a927ffe4f2dd602ffbe456320328361c93f9b7
angeonseok <dudwltjs@gmail.com>
2026-03-18 17:25:38 +0900

    1

--- 백준/25.이분 탐색(완)/12015.가장 긴 증가하는 부분 수열 2.py
+++ 백준/25.이분 탐색(완)/12015.가장 긴 증가하는 부분 수열 2.py
@@ -11,10 +11,25 @@
 """
 
 import sys
+from bisect import bisect_left
 input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
 
-temp = []
+#정답을 담을 리스트
+ans = []
+for x in arr:
 
+    # x가 ans에서 어디 들어가나 보자
+    temp = bisect_left(ans, x)
+
+    #ans 끝에 들어가야 되면 길이 늘어나는거라 추가
+    if temp >= len(ans):
+        ans.append(x)
+
+    #아니면 기존값 갱신
+    else:
+        ans[temp] = x
+
+print(len(ans))
--- 백준/38.스택, 큐, 덱 2/17298.오큰수.py
+++ 백준/38.스택, 큐, 덱 2/17298.오큰수.py
@@ -13,3 +13,21 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+arr = list(map(int, input().split()))
+
+#일단 -1로 깔아
+ans = [-1] * n
+
+#인덱스 저장용
+stack = []
+
+for i in range(n):
+
+    # 현재 값이 더 크면 스택에서 꺼내면서 정답 채움
+    while stack and arr[stack[-1]] < arr[i]:
+        ans[stack.pop()] = arr[i]
+    
+    #아직 처리 안된 인덱스 스택에 넣어
+    stack.append(i)
+    
+print(*ans)
