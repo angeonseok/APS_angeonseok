92f733626c17a7c1dba7636d0445b3ddf24e74fa
angeonseok <dudwltjs@gmail.com>
2026-03-24 16:11:01 +0900

    1

--- 백준/38.스택, 큐, 덱 2/1725.히스토그램.py
+++ 백준/38.스택, 큐, 덱 2/1725.히스토그램.py
@@ -20,5 +20,27 @@ arr = [int(input()) for _ in range(n)]
 stack = []
 ans = 0
 
+
 for i in range(n):
-    
+    #현재 막대의 시작지점임
+    start = i
+
+    #현재 막대보다 더 큰 놈들은 넓이 계산하고 끝내
+    while stack and stack[-1][1] > arr[i]:
+        a, value = stack.pop()
+
+        # 지금 높이는 더 낮은 놈
+        # 방금 뽑은 막대 시작점까지 먹고 들어갈 수 있음
+        start = a
+        ans = max(ans, value * (i - start))
+
+    #시작지점이랑 높이 같이 넣기
+    stack.append((start, arr[i]))
+
+#계산 안된 위치들 전부 계산하기
+#맨 오른쪽에서 막대까지 거리를 밑변으로 잡으면 됨
+while stack:
+    start, value = stack.pop()
+    ans = max(ans, value * (n - start))
+
+print(ans)
